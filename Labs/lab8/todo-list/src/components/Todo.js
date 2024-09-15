5c895e04d971763a5aae212a53466353c7807ce4
Kosiso Abone <abonekosiso@gmail.com>
2024-09-14 23:44:35 -0400

    added stated and functions for the todos

--- Labs/lab8/todo-list/src/components/Todo.js
+++ Labs/lab8/todo-list/src/components/Todo.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Todo() {
+    const [todos, setTodos] = useState([]);
+    const [newTodo, setNewTodo] = useState('');
+
+    const addTodos = () => {
+        if(newTodo === '') return;
+        setTodos([...todos, newTodo]);
+        setNewTodo('');
+    }
+
+    const removeTodo = (index) => {
+        const updatedTodos = [...todos];
+        updatedTodos.splice(index, 1);
+        setTodos(updatedTodos);
+    }
+
   return (
     <div style={{textAlign: 'center'}}>
         <h1 style={{textAlign: 'center', color: 'darkblue'}}>My Todo List</h1>
